94c5601af851d2de0736a4493c0310450906869a
admrid <adam.ridovics@gmail.com>
2017-11-27 12:44:46 +0100

    exercise2 solved

---
+++ week-02/day-1/02-humpty_dumpty.py
@@ -0,0 +1,6 @@
+
+# Modify this program to print Humpty Dumpty riddle correctly
+print("Humpty Dumpty sat on a wall,")
+print("Humpty Dumpty had a great fall.")
+print("All the king's horses and all the king's men")
+print("Couldn't put Humpty together again.")
