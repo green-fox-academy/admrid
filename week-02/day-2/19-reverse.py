234851a6368ab6456b9492624177ea15b577e803
admrid <adam.ridovics@gmail.com>
2017-12-01 00:05:09 +0100

    fixed mistakes

--- week-02/day-2/19-reverse.py
+++ week-02/day-2/19-reverse.py
@@ -4,7 +4,18 @@
 # - Print the elements of the reversed `aj`
 
 aj = [3, 4, 5, 6, 7]
-rj = 0
-for i in range(len(aj) - 1, -1, -1):
-    rj.append(i)
-print(aj = rj)
+# rj = 0
+# for i in range(len(aj) - 1, -1, -1):
+#     rj.append(i)
+# print(aj = rj)
+
+aj[4], aj[3], aj[2], aj[1], aj[0] = aj[0], aj[1], aj[2], aj[3], aj[4]
+print(aj)
+
+# OR:
+# ja = []
+
+# for i in aj[::-1]:
+#     ja.append(i)
+# aj = ja
+# print(aj)
