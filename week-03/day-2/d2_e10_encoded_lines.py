d0cd7e52ce9ac8bb6cdf2811629e6c014b7f5183
admrid <adam.ridovics@gmail.com>
2017-12-05 22:08:24 +0100

    exercise10 solved

--- week-03/day-2/d2_e10_encoded_lines.py
+++ week-03/day-2/d2_e10_encoded_lines.py
@@ -1,15 +1,35 @@
 # Create a method that decrypts encoded-lines.txt
+# def decrypt(file_name):
+#     textfile = open(file_name)
+#     textdec = open('file10dec.txt', 'w')
+#     textorig = textfile.read()
+#     text = ''
+#     for i in textorig:
+#         charval = ord(i)
+#         text += chr(charval - 1)
+#     textdec.write(text)
+#     textfile.close
+#     textdec.close
+#     return text
+
+# print(decrypt('file10.txt'))
+
 def decrypt(file_name):
     textfile = open(file_name)
     textdec = open('file10dec.txt', 'w')
-    textorig = textfile.read()
-    text = ''
-    for i in textorig:
-        charval = ord(i)
-        text += chr(charval - 1)
-    textdec.write(text)
+    textlist = textfile.readlines()
+    # miért nem definiálhatom ezt itt?: textlines = ''
+    for line in textlist:
+        textlines = ''
+        for char in line:
+            if char == ' ':
+                textlines += ' '
+            else:
+                charval = ord(char)
+                textlines += chr(charval - 1)
+        textdec.write(textlines + '\n')
     textfile.close
     textdec.close
-    return text
+    return 'Yes'
 
 print(decrypt('file10.txt'))
