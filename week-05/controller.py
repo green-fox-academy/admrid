44a7e4e203389357229d2498b8295831d988ca03
admrid <adam.ridovics@gmail.com>
2017-12-20 14:56:02 +0100

    hero faces the direction in which he is going

--- week-05/controller.py
+++ week-05/controller.py
@@ -1,5 +1,8 @@
 from model import *
 from view import *
+# import gc
+
+# gc.disable()
 
 map = Map()
 hero = Hero()
@@ -12,21 +15,32 @@ for i in range(len(map.map_lista)):
 
 def on_key_press(e):
     # print(e)
-    if e.keysym == 'Up':
-        hero.move('up')
-        printer.canvas.move(hero.imagefile, 0, 72)        
+    hero_old_y = hero.y
+    hero_old_x = hero.x
+
+    if e.keysym == 'Up':    
+        hero.move('up')        
+        # printer.drawer(hero)
+        printer.canvas.itemconfig(printer.hero_img, image = printer.hero_up)
+        printer.canvas.move(printer.hero_img, hero.x - hero_old_x, hero.y - hero_old_y)
+        # printer.canvas.move('myhero', hero.x - hero_old_x, hero.y - hero_old_y)
+        # printer.canvas.move('myhero', 0, -72)        
         # print('up')
     elif e.keysym == 'Down':
         hero.move('down')
-        printer.canvas.move(hero.imagefile, 0, -72)        
+        # printer.drawer(hero)
+        printer.canvas.itemconfig(printer.hero_img, image = printer.hero_down)
+        printer.canvas.move(printer.hero_img, hero.x - hero_old_x, hero.y - hero_old_y)      
         # print('down')
     elif e.keysym == 'Right':
         hero.move('right')
-        printer.drawer(hero)
+        printer.canvas.itemconfig(printer.hero_img, image = printer.hero_right)
+        printer.canvas.move(printer.hero_img, hero.x - hero_old_x, hero.y - hero_old_y)
         # print('right')
     elif e.keysym == 'Left':
         hero.move('left')
-        printer.drawer(hero)
+        printer.canvas.itemconfig(printer.hero_img, image = printer.hero_left)
+        printer.canvas.move(printer.hero_img, hero.x - hero_old_x, hero.y - hero_old_y)
         # print('left')
 
 
--- week-05/view.py
+++ week-05/view.py
@@ -1,7 +1,8 @@
 from tkinter import *
 from PIL import ImageTk, Image
+from model import *
 
-images = []
+
 
 class Printer():
     def __init__(self):
@@ -12,18 +13,35 @@ class Printer():
 
         self.canvas.focus_set()
 
+        self.image_obj = None
+
+        # HERO IMAGES LOADED:
+        self.hero_down = ImageTk.PhotoImage(file = 'assets/hero-down.gif')
+        self.hero_up = ImageTk.PhotoImage(file = 'assets/hero-up.gif')
+        self.hero_left = ImageTk.PhotoImage(file = 'assets/hero-left.gif')
+        self.hero_right = ImageTk.PhotoImage(file = 'assets/hero-right.gif')
+
+        self.images = []
+        self.hero_img = None
+
     # load the .gif image file
     # imagefile_to_draw = PhotoImage(file='assets/floor.gif')
     # imagefile_to_draw = PhotoImage(file=imagefile)
     # opened_image = Image.open(imagefile)
 
     def drawer(self, thing_to_draw):
-        opened_image = Image.open(thing_to_draw.imagefile)
-        image_obj = ImageTk.PhotoImage(opened_image)
-        images.append(image_obj)   
-        # put gif image on canvas
-        # pic's upper left corner (NW) on the canvas is at x=50 y=10
-        self.canvas.create_image(thing_to_draw.x, thing_to_draw.y, anchor=NW, image=image_obj)
-        self.canvas.update()
+        # print(issubclass(thing_to_draw, Tile))
+        if isinstance(thing_to_draw, Tile):
+            opened_image = Image.open(thing_to_draw.imagefile)
+            image_obj = ImageTk.PhotoImage(opened_image)
+            self.images.append(image_obj)
+            self.canvas.create_image(thing_to_draw.x, thing_to_draw.y, anchor=NW, image=image_obj)
+            self.canvas.update()
+        
+        elif isinstance(thing_to_draw, Hero):
+            opened_image = Image.open(thing_to_draw.imagefile)
+            self.image_obj = ImageTk.PhotoImage(opened_image)
+            self.hero_img = self.canvas.create_image(thing_to_draw.x, thing_to_draw.y, anchor=NW, image=self.image_obj, tags='myhero')
+            self.canvas.update()
 
     # root.mainloop()
