833cfff7769ba743fdc1c00271b86b4d1c2e3d60
admrid <adam.ridovics@gmail.com>
2018-01-08 12:34:25 +0100

    exercise1-7 are ready

---
+++ week-07/d1-e5.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let a = 22;
+let b = 13;
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+// Print the result of 13 added to 22
+console.log(b + a);
+// Print the result of 13 substracted from 22
+console.log(a - b);
+// Print the result of 22 mutiplied by 13
+console.log(a * b);
+// Print the result of 22 divided by 13 (as a decimal fraction)
+console.log(a / b);
+// Print the reminder of 22 divided by 13
+console.log(a % b);
