76a05b50dc18557b580a25be5b34687e62b9e1b3
admrid <adam.ridovics@gmail.com>
2018-01-08 15:05:34 +0100

    exercise22-24 are ready

---
+++ week-07/d1-e23.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+for (var i = 1; i < 101; i++) {
+  console.log('I wont\'t cheet on the exam!');
+}
---
+++ week-07/d1-e24.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that prints all the even numbers between 0 and 500
+for (var i = 0; i < 501; i++) {
+  if (i % 2 === 0) {
+    console.log(i)
+  }
+}
