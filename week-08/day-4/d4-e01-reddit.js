74a8afad3922c8d4f84d8eb630571dc57e22b057
admrid <adam.ridovics@gmail.com>
2018-01-21 16:28:44 +0100

    reddit posting is working

---
+++ week-08/day-4/d4-e01-reddit.js
@@ -0,0 +1,60 @@
+'use strict';
+
+window.onload = function() {
+
+  let redditReq = new XMLHttpRequest();
+  let redditPosts;
+
+  redditReq.open("GET", "https://time-radish.glitch.me/posts");
+  redditReq.send();
+
+  let building = function(toProcess) {
+    // let redditPostsList = document.createElement('ul');
+    // document.body.appendChild(redditPostsList);
+    toProcess.forEach(post => { 
+      let mainDiv = document.querySelector('main');
+      let redditPost = document.createElement('section');
+      let redditPostVote = document.createElement('section');
+      let redditPostVoteUp = document.createElement('button');
+      let redditPostVoteCount = document.createElement('p');
+      let redditPostVoteDown = document.createElement('button');
+      let redditPostContent = document.createElement('section');
+      let redditPostHeader = document.createElement('h2');
+      let redditPostDate = document.createElement('p');
+      // let redditPostLink = document.createElement('a');
+      // let redditPostUser = document.createElement('a');
+      
+      redditPostVoteCount.innerText = post.score;
+      redditPostHeader.innerHTML = `<a href="${post.url}">${post.title}</a>`;
+      redditPostDate.innerHTML = `submitted ${post.timestamp} ago by <a href="">${post.owner}</a>`;
+
+      mainDiv.classList.add('main');
+      redditPost.classList.add('post');
+      redditPostVote.classList.add('vote');
+      redditPostVoteUp.classList.add('upvote');
+      redditPostVoteDown.classList.add('downvote');
+      redditPostVoteCount.classList.add('voteCount');
+      redditPostContent.classList.add('post-content');
+
+      mainDiv.appendChild(redditPost);
+      redditPost.appendChild(redditPostVote);
+      redditPost.appendChild(redditPostContent);
+      redditPostVote.appendChild(redditPostVoteUp);
+      redditPostVote.appendChild(redditPostVoteCount);
+      redditPostVote.appendChild(redditPostVoteDown);
+      redditPostContent.appendChild(redditPostHeader);
+      redditPostContent.appendChild(redditPostDate);
+    });
+  };
+
+  redditReq.onreadystatechange = function() {
+    if (redditReq.readyState === XMLHttpRequest.DONE && redditReq.status === 200) {
+      redditPosts = JSON.parse(redditReq.response);
+      // console.log(redditReq);
+      console.log('posts.response: ', redditPosts.response);
+      console.log('posts: ', redditPosts);
+      console.log('posts posts: ', redditPosts.posts);
+      building(redditPosts.posts);
+    }
+  };
+};
