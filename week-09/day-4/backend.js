e5b44d3e85724578b6aed5ee73d28df012059f51
admrid <adam.ridovics@gmail.com>
2018-01-25 17:29:54 +0100

    doubling exercise is ready

--- week-09/day-4/backend.js
+++ week-09/day-4/backend.js
@@ -6,11 +6,30 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static('assets')); 
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 }); 
+
+app.get('/doubling', function (req, res) {
+  if (req.query.input === undefined) {
+    res.json(
+      {
+        error: 'Please provide an input!'
+      }
+    );
+  } else {
+    res.json(
+      {
+        received: req.query.input,
+        result: req.query.input * 2
+      }
+    );
+  }
+});
+
+
 
 
 app.listen(8080, function() {
